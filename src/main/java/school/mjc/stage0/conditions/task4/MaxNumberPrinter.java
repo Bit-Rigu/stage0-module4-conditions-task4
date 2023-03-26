91146e18a171fe478322f42c837c6b20f404959f
Dmitrii Bit-Rigu <daxudo@gmail.com>
2023-03-26 17:48:09 +0300

    Solving the tasks.

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (second >= first){
+            System.out.println(
+                    third >= second ? third : second);
+        } else {
+            System.out.println(
+                    third >= first ? third : first);
+        }
     }
 }
